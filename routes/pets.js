47bda0bbe00c9ad89a30c47c555dab4ff182504d
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-25 10:19:00 -0800

    updated create route that can handle image url

--- routes/pets.js
+++ routes/pets.js
@@ -34,22 +34,32 @@ module.exports = (app) => {
   // INDEX PET => index.js
 
   // NEW PET
-  app.get('/pets/new', (req, res) => {
+  app.get('/pets/new',  (req, res) => {
     res.render('pets-new');
   });
 
   // CREATE PET
-  app.post('/pets', (req, res) => {
+  app.post('/pets', upload.single('avatar'), (req, res, next) => {
     var pet = new Pet(req.body);
+    pet.save(function (err) {
+      if (req.file) {
+        client.upload(req.file.path, {}, function (err, versions, meta) {
+          if (err) { return res.status(400).send({ err: err }) };
 
-    pet.save()
-      .then((pet) => {
+          versions.forEach(function (image) {
+            var urlArray = image.url.split('-');
+            urlArray.pop();
+            var url = urlArray.join('-');
+            pet.avatarUrl = url;
+            pet.save();
+          });
+
+          res.send({ pet: pet });
+        });
+      } else {
         res.send({ pet: pet });
-      })
-      .catch((err) => {
-        // STATUS OF 400 FOR VALIDATIONS
-        res.status(400).send(err.errors);
-      });
+      }
+    })
   });
 
   // SHOW PET
