7c426c4c22e21d9aad96681f372e92424ebe701d
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-24 20:41:27 -0800

    using countPage in the route and while loop in index pug

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ module.exports = (app) => {
 
   /* GET home page. */
   app.get('/', (req, res) => {
-    Pet.paginate().then((results) => {
-      res.render('pets-index', { pets: results.docs });
+    // current page
+    const page = req.query.page || 1
+
+    Pet.paginate({}, { page: page }).then((results) => {
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages });
     });
   });
 }
