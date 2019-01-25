606e20a6583dd0ed5caa716081bcdfc523354baa
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-24 21:23:43 -0800

    validation in pet model added

--- models/pet.js
+++ models/pet.js
@@ -7,17 +7,16 @@ mongoosePaginate.paginate.options = {
   limit: 3 // how many records on each page
 };
 const PetSchema = new Schema({
-    name            : { type: String, required: true }
-  , species         : { type: String }
-  , birthday        : { type: Date }
-  , picUrl          : { type: String }
-  , picUrlSq        : { type: String }
-  , favoriteFood    : { type: String }
-  , description     : { type: String }
-},
-{
-  timestamps: true
-});
+  name: { type: String, required: true }
+  , birthday: { type: String, required: true }
+  , species: { type: String, required: true }
+  , picUrl: { type: String, required: true }
+  , picUrlSq: { type: String, required: true }
+  , favoriteFood: { type: String, required: true }
+  , description: { type: String, minlength: 140, required: true }
+}, {
+    timestamps: true
+  });
 
 PetSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model('Pet', PetSchema);
