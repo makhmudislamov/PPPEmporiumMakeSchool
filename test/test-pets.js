88f3379460cdbb5c0cdeeafab93c57bb7e66cca3
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-29 16:27:39 -0800

    updated index test

--- test/test-pets.js
+++ test/test-pets.js
@@ -26,14 +26,16 @@ describe('Pets', ()  => {
   });
 
   // TEST INDEX
-  it('should index ALL pets on / GET', (done) => {
+  it('should list ALL pets on /pets GET', function (done) {
     chai.request(server)
-        .get('/')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.html;
-          done();
-        });
+      .get('/')
+      .set('content-type', 'application/json')
+      .end(function (err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+      });
   });
 
   // TEST NEW
@@ -129,3 +131,4 @@ it('should search ALL pets by name on /search GET', (done) => {
       done();
     });
 });
+
