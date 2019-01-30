0971c5cfd7be7397abdcf11dee7d6ad9fc76533e
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-29 16:21:33 -0800

    detecting json requests

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,12 @@ module.exports = (app) => {
     const page = req.query.page || 1
 
     Pet.paginate({}, { page: page }).then((results) => {
-      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      // res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      if (req.header('content-type') == 'application/json') {
+        return res.json({ pets: pets, categories: categories });
+      } else {
+        return res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      }
     });
   });
 }
